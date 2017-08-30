d47bffef5693e2ed2df89308b868edd25feee3ea
Christopher Dickson <chrisd1100@gmail.com>
2017-08-30 19:06:54 +0000

    routine to draw stacked charts

--- greedo.js
+++ greedo.js
@@ -82,3 +82,36 @@ function google_draw_line(data, cols, element_id, title, width, height)
 		}));
 	});
 }
+
+function google_draw_stacked(data, cols, element_id, title, width, height)
+{
+	google.charts.load('current', {'packages':['bar']});
+	google.charts.setOnLoadCallback(function () {
+
+		var datat = new google.visualization.DataTable();
+
+		datat.addColumn('string', 'Day');
+
+		for (var x = 0; x < cols.length; x++)
+			datat.addColumn('number', cols[x]);
+
+		datat.addRows(data);
+
+		var formatter = new google.visualization.NumberFormat({pattern:'0.00'});
+		for (var x = 0; x < cols.length; x++)
+			formatter.format(datat, x + 1);
+
+		formatter = new google.visualization.DateFormat({pattern:'YYYY-MM-DD'});
+		formatter.format(datat, 0);
+
+		var chart = new google.charts.Bar(document.getElementById(element_id));
+		chart.draw(datat, google.charts.Bar.convertOptions({
+			chart: {
+				title: title,
+			},
+			width: width,
+			height: height,
+			isStacked: 'relative'
+		}));
+	});
+}
